ce593acdf20c751b48fdbb52db81616fc86135c5
GitRedFlower <theredflower19999@gmail.com>
2025-12-03 17:02:47 -0500

    Fixed Issue #4 https://github.com/GitRedFlower/Verse/issues/4

--- src/main/java/net/redflower/verse/item/custom/VerseFoodProperties.java
+++ src/main/java/net/redflower/verse/item/custom/VerseFoodProperties.java
@@ -8,45 +8,45 @@ public class VerseFoodProperties {
 
     //Ore Apples
     public static final FoodProperties COAL_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.NIGHT_VISION, 2400, 0), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.NIGHT_VISION, 2400, 0), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties COPPER_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400, 0), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400, 0), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties IRON_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2400, 0), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2400, 0), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties REDSTONE_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.DIG_SPEED, 2400, 0), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.DIG_SPEED, 2400, 0), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties LAPIS_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.WATER_BREATHING, 2400, 0), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.WATER_BREATHING, 2400, 0), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties EMERALD_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 2400, 1), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 2400, 1), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties DIAMOND_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.REGENERATION, 2400, 0), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.REGENERATION, 2400, 0), 1.0f).alwaysEdible().build();
 
     //Enchanted Ore Apple
     public static final FoodProperties ENCHANTED_COAL_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.NIGHT_VISION, 2400, 2), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.NIGHT_VISION, 2400, 2), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties ENCHANTED_COPPER_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400, 2), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400, 2), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties ENCHANTED_IRON_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2400, 2), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2400, 2), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties ENCHANTED_REDSTONE_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.DIG_SPEED, 2400, 2), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.DIG_SPEED, 2400, 2), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties ENCHANTED_LAPIS_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.WATER_BREATHING, 2400, 2), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.WATER_BREATHING, 2400, 2), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties ENCHANTED_EMERALD_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 2400, 3), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 2400, 3), 1.0f).alwaysEdible().build();
 
     public static final FoodProperties ENCHANTED_DIAMOND_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.75f)
-            .effect( () -> new MobEffectInstance(MobEffects.REGENERATION, 2400, 2), 1.0f).build();
+            .effect( () -> new MobEffectInstance(MobEffects.REGENERATION, 2400, 2), 1.0f).alwaysEdible().build();
 }
